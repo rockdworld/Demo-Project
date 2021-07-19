8b57edf85a88484b8fb4bf07e1eeec6678605dd2
Manish <manishagrawal166@gmail.com>
2021-07-19 20:50:57 +0530

    Added Home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomeComponent},
 
 ];
 
