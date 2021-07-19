7e2ef9555ab6b98499e2a020799fae298644a01d
Manish <manishagrawal166@gmail.com>
2021-07-19 20:56:43 +0530

    Added Reports module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ReportsComponent } from './reports/reports.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
+  {path: 'reports', component: ReportsComponent},
 
 ];
 
